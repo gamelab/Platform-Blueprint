d18d25df0638595f02a42939be7905eda940b5f1
Phil Wickliffe <phillip.wickliffe@gmail.com>
2014-04-17 15:37:07 +1200

    Basic sloping calculation

--- src/States/Platformer-1.0.0.js
+++ src/States/Platformer-1.0.0.js
@@ -33,6 +33,11 @@ PlatformState.create = function () {
     this.jumps = 0;
     //catch if up key is still down
     this.jumpReleased = true;
+    this.sloping = true;
+    //catch if left button is currently pressed
+    this.leftDown = false;
+    //catch if right button is currently pressed
+    this.rightDown = false;
 
     this.player = new PlayerSprite(this, this.textures.player, 0, 0);
     this.player.animation.add('walking', [1, 2, 3, 4, 5, 6], 0.1, true);
@@ -43,6 +48,9 @@ PlatformState.create = function () {
 
     //Add to the screen.
     this.addChild(this.player);
+
+    this.px = this.player.x + this.player.box.bounds.width / 2;
+    this.py = this.player.y + this.player.height;
 
     this.generateForegroundTileMap();
 
@@ -109,6 +117,9 @@ PlatformState.update = function () {
     
     Kiwi.State.prototype.update.call(this);
 
+    //reset key movement
+    this.leftDown = false;
+    this.rightDown = false;
     //move character via keyboard input
     if (this.game.input.keyboard.isDown(Kiwi.Input.Keycodes.LEFT)) {
         this.player.scaleX = -1;
@@ -122,48 +133,35 @@ PlatformState.update = function () {
         this.player.physics.velocity.x = 0;
     }
 
+    //check sloping collisions
+    this.sloping = false;
+
     //CHECK TILES
+    //round the player position to make tile calculation easier
+    this.player.x = Math.round(this.player.x);
+    this.player.y = Math.round(this.player.y);
     //bottom middle point of player to check against sloping tiles
-    var px = this.player.x + this.player.box.bounds.width / 2;
-    var py = this.player.y + this.player.height;
+    this.px = Math.round(this.player.x + this.player.box.bounds.width / 2);
+    this.py = this.player.y + this.player.height;
 
-    //overlap slope left
-    var sloping = false;
-    var leftSlopes = this.slopeLeftLayer.getOverlappingTiles(this.player);
-    for (var i = 0; i < leftSlopes.length; i++) {
-        var slope = leftSlopes[i];
-        //check to see if within bounds
-        if (px > slope.x && px < slope.x + this.tileWidth) {
-            var slopeX = px - slope.x;
-            var slopeY = py - slope.y;
-            if (slopeY >= this.tileHeight - slopeX) {
-                this.player.y = slope.y + this.tileHeight - slopeX - this.player.height;
-                sloping = true;
-            }
+    this.checkLeftSlope();
+    this.checkRightSlope();
+    
+    //ifthe player is not on a slope, check for regular tile collision
+    if (!this.sloping) {
+        //overlap ground
+        this.groundLayer.physics.overlapsTiles(this.player, true);
+        //Are we on the ground?
+        if (this.player.physics.isTouching(Kiwi.Components.ArcadePhysics.DOWN)) {
+            //console.log('ground')
+            this.jumps = 0;
+        } else if (this.jumps == 0) {
+            //if the player walks off an edge, call it a jump
+            this.jumps = 1;
         }
-    }
-
-    //overlap slope right
-    var rightSlopes = this.slopeRightLayer.getOverlappingTiles(this.player);
-    for (var i = 0; i < rightSlopes.length; i++) {
-        var slope = rightSlopes[i];
-        if (px > slope.x && px < slope.x + this.tileWidth) {
-            var slopeX = px - slope.x;
-            var slopeY = py - slope.y;
-            if (slopeY >= slopeX) {
-                this.player.y = slope.y + slopeX - this.player.height;
-                sloping = true;
-            }
-        }
-    }
-
-    //overlap ground
-    this.groundLayer.physics.overlapsTiles(this.player, true);
-    //Are we on the ground?
-    if (this.player.physics.isTouching(Kiwi.Components.ArcadePhysics.DOWN)) {
-        //console.log('ground')
+    } else {
+        //on ground anyway, so reset jumps
         this.jumps = 0;
-        
     }
     if (this.game.input.keyboard.isDown(Kiwi.Input.Keycodes.UP)) {
         this.jump();
@@ -172,6 +170,11 @@ PlatformState.update = function () {
     }
 }
 
+/**
+* This method makes the player jump when the Up key is pressed. Can jump multiple times based on the variale 'jumpCount'
+* @method jump
+* @public
+*/
 PlatformState.jump = function () {
     if (this.jumps < this.jumpCount && this.jumpReleased) {
         this.player.physics.velocity.y = -100;
@@ -181,3 +184,130 @@ PlatformState.jump = function () {
     }
 }
 
+
+/**
+* This method checks to see if a player is on a leftSlope.
+*
+* Please note, "leftSlope" is refering to a slope that when the player is standing on the tile, facing away from the tile, the player is facing left.
+* Also note this function checks for the outward edges of the sloping tile and calculates those points too for a more polished slope interaction
+*
+* @method checkLeftSlope
+* @public
+*/
+PlatformState.checkLeftSlope = function () {
+    var tx = Math.floor(this.px / this.tileWidth);
+    var ty = Math.floor(this.py / this.tileHeight);
+
+    //basic slope tile collision, entering a slope from the top
+    var slope = this.slopeLeftLayer.getTileFromXY(tx, ty);
+    if (slope != null) {
+        //if a tile exists, check collision
+        if (slope.index != 0) {
+            //get the slope postion
+            var slopeX = tx * this.tileWidth;
+            var slopeY = ty * this.tileHeight;
+            this.sloping = true;
+
+            //get the difference of player position ad slope 0,0 co-ordinates
+            var diffX = this.px - slopeX;
+            var diffY = this.py - slopeY;
+
+            //if you're within the solid part of a sloping tile
+            //OR if youve just entered a tile from the right, snap down into the tile
+            if ((diffY >= this.tileHeight - diffX)
+            || (this.px - (this.player.physics.velocity.x / 10) >= slopeX + this.tileWidth)) {
+                var destY = slopeY + this.tileHeight - diffX;
+
+                this.player.y = Math.ceil(destY - this.player.height);
+                this.player.physics.velocity.y = 40;
+
+                //already sloping, so no need for further checking
+                return;
+            }
+        }
+    }
+
+    //check when entering onto a sloping tile, and also when between two, or when you'ce fallen "into" the tile
+    var aboveSlope = this.slopeLeftLayer.getTileFromXY(tx, ty-1);
+    if (aboveSlope != null) {
+        //if a tile exists, check collision
+        if (aboveSlope.index != 0) {
+            var aboveSlopeX = tx * this.tileWidth;
+            var aboveSlopeY = (ty-1) * this.tileHeight;
+
+            //flag to ignore remaining map collision in the update loop
+            this.sloping = true;
+
+            var diffX = this.px - aboveSlopeX;
+            var destY = (aboveSlopeY + this.tileHeight) - diffX;
+
+            this.player.y = destY - this.player.height;
+            this.player.physics.velocity.y = 40;
+        }
+    }
+}
+
+/**
+* This method checks to see if a player is on a rightSlope.
+*
+* Please note, "rightSlope" is refering to a slope that when the player is standing on the tile, facing away from the tile, the player is facing right.
+* Also note this function checks for the outward edges of the sloping tile and calculates those points too for a more polished slope interaction
+*
+* @method checkRightSlope
+* @public
+*/
+PlatformState.checkRightSlope = function () {
+    if (this.sloping) {
+        //console.log('Already sloping on left slope')
+        return;
+    }
+    //get the bottom centre point ofthe character to calculate position from
+    //tile based location
+    var tx = Math.floor(this.px / this.tileWidth);
+    var ty = Math.floor(this.py / this.tileHeight);
+
+    var slope = this.slopeRightLayer.getTileFromXY(tx, ty);
+    if (slope != null) {
+        //if a tile exists, check collision
+        if (slope.index != 0) {
+            //get the slope postion
+            var slopeX = tx * this.tileWidth;
+            var slopeY = ty * this.tileHeight;
+
+            //flag to ignore remaining map collision in the update loop
+            this.sloping = true;
+
+            //get the difference of player position ad slope 0,0 co-ordinates
+            var diffX = this.px - slopeX;
+            var diffY = this.py - slopeY;
+
+            //if you're within the solid part of a sloping tile
+            //OR if youve just entered a tile from the left, snap down into the tile
+            if ((diffY >= diffX)
+            || (this.px - (this.player.physics.velocity.x / 10) <= slopeX)) {
+                this.player.y = Math.ceil(slopeY + diffX - this.player.height);
+                this.player.physics.velocity.y = 40;
+
+                //already sloping, so no need for further checking
+                return;
+            }
+        }
+    }
+
+    //check when entering onto a sloping tile, or when you've fallen "into" the tile
+    var aboveSlope = this.slopeRightLayer.getTileFromXY(tx, ty - 1);
+    if (aboveSlope != null) {
+        //if a tile exists, check collision
+        if (aboveSlope.index != 0) {
+            //flag to ignore remaining map collision in the update loop
+            this.sloping = true;
+
+            var aboveSlopeX = tx * this.tileWidth;
+            var aboveSlopeY = (ty - 1) * this.tileHeight;
+            var diffX = this.px - aboveSlopeX;
+
+            this.player.y = Math.ceil(aboveSlopeY + diffX - this.player.height);
+            this.player.physics.velocity.y = 40;
+        }
+    }
+}
